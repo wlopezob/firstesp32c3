79c6699060929d1c61731cb2b77d299e82beed4e
william luis lopez obregon <wlopezob@gmail.com>
2023-01-20 01:45:24 +0000

    remove

--- target/debug/deps/rust_first_api-317010deadc210f8.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rust_first_api/target/debug/deps/rust_first_api-317010deadc210f8: src/main.rs
-
-/workspaces/rust_first_api/target/debug/deps/rust_first_api-317010deadc210f8.d: src/main.rs
-
-src/main.rs:
--- target/debug/rust_first_api.d
+++
@@ -1,1 +0,0 @@
-/workspaces/rust_first_api/target/debug/rust_first_api: /workspaces/rust_first_api/src/main.rs
